7b7406338c60ba49b96cf1475a89bf12d994b9a0
zahmis <meda829ratteru86@gmail.com>
2023-04-21 00:31:39 +0900

    update

--- background.js
+++ background.js
@@ -4,8 +4,33 @@ chrome.contextMenus.create({
   contexts: ["selection"],
 });
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
   if (info.menuItemId === "textSpacer") {
-    chrome.tabs.sendMessage(tab.id, { action: "processSelectedText" });
+    try {
+      chrome.tabs.sendMessage(
+        tab.id,
+        { action: "processSelectedText" },
+        (response) => {
+          if (chrome.runtime.lastError) {
+          } else {
+            console.log("Message sent successfully:", response);
+          }
+        }
+      );
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
   }
 });
+
+function insertSpaceBetweenLanguages(text) {
+  text = text.replace(
+    /([a-zA-Z])([\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF])/g,
+    "$1 $2"
+  );
+  text = text.replace(
+    /([\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF])([a-zA-Z])/g,
+    "$1 $2"
+  );
+  return text;
+}
--- content.js
+++ content.js
@@ -4,7 +4,11 @@ function insertSpaceBetweenLanguages(text) {
     "$1 $2"
   );
   text = text.replace(
-    /([\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF])([a-zA-Z])/g,
+    /([\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF])([a-zA-Z0-9])/g,
+    "$1 $2"
+  );
+  text = text.replace(
+    /([a-zA-Z0-9])([\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF])/g,
     "$1 $2"
   );
   return text;
@@ -23,5 +27,6 @@ function processSelectedText() {
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "processSelectedText") {
     processSelectedText();
+    sendResponse({ success: true });
   }
 });
